ef505972bd2ac1487e0d0e30293fa288ad68d1f3
uta0463 <xxxuta0463xxx@gmail.com>
2024-10-25 15:13:18 +0900

    update files

--- next.config.ts
+++ next.config.ts
@@ -2,10 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  webpack: (config) => {
-    config.resolve.alias['leaflet'] = 'leaflet/dist/leaflet.js';
-    return config;
-  },
+  output: 'export',
 };
 
 export default nextConfig;
